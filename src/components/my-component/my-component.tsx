dd84b5a2a8c8bb6f27254191072ca638f2042201
Chris Braddock <chris.braddock@mastercard.com>
2018-08-22 06:49:57 -0400

    dotenv not working
    suspect this has more to do with webpack vs. rollup than anything else

--- src/components/my-component/my-component.tsx
+++ src/components/my-component/my-component.tsx
@@ -1,4 +1,7 @@
 import { Component, Prop } from '@stencil/core';
+import dotenv from 'dotenv'
+dotenv.config({ path: '/Users/e071111/Documents/repos/stencil-module-issue/src/.env' })
+// require('dotenv').config()
 
 @Component({
   tag: 'my-component',
@@ -11,6 +14,9 @@ export class MyComponent {
   @Prop() last: string;
 
   render() {
+    console.log('>>>>>');
+    console.log(process.env.FOO)
+    console.log('>>>>>');
     return (
       <div>
         Hello, World! I'm {this.first} {this.last}
